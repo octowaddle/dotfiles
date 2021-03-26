4b421bc238b763b76d3ced9d4e4c43eb91068498
Daniel Wolbach <octowaddle@users.noreply.github.com>
2021-03-26 13:05:42 +0100

    Fix

--- apply.py
+++ apply.py
@@ -9,8 +9,8 @@ delete_existing = False
 
 # List files
 files = [".vim/vimrc", ".bashrc", ".gitconfig", ".config/nvim/init.vim",
-         ".config/alacritty/alacritty.yml", ".config/bspwm/bspwmrc", 
-         ".config/bspwm/scripts/resize.sh", ".config/sxhkd/sxhkdrc",+
+         ".config/alacritty/alacritty.yml", ".config/bspwm/bspwmrc",
+         ".config/bspwm/scripts/resize.sh", ".config/sxhkd/sxhkdrc",
          ".config/picom/picom.conf", ".config/polybar/config"]
 
 # Arguments
