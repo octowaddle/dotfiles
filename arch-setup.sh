8568501b13664e72c5b127d0443e671fbce40d08
Daniel Wolbach <77749247+octowaddle@users.noreply.github.com>
2021-07-05 22:42:49 +0200

    feat: add audio via pipewire

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 # Set up
 #
 
 # Install Paru
-sudo pacman -S --needed git base-devel
+sudo pacman -Sy --needed --noconfirm git base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
@@ -17,18 +17,14 @@ rm -rf paru
 #
 
 # Basic software
-paru -S --needed neovim bspwm sxhkd polybar picom-git playerctl pamixer\
-                 alacritty bat exa clang fish fzf meson ninja ripgrep rofi\
-                 tldr xorg-setxkbmap xorg-xset xorg-xsetroot fd\
-                 clang-tools-extra nodejs npm xorg-xinit xorg-server man-db\
-                 man-pages sudo nerd-fonts-jetbrains-mono\
-                 visual-studio-code-bin python3 python-pip firefox github-cli
+paru -Sy --needed --noconfirm neovim bspwm sxhkd polybar picom-git playerctl pamixer alacritty bat exa clang fish fzf meson ninja ripgrep rofi tldr xorg-setxkbmap xorg-xset xorg-xsetroot fd clang-tools-extra nodejs npm xorg-xinit xorg-server man-db man-pages sudo nerd-fonts-jetbrains-mono visual-studio-code-bin python3 python-pip firefox github-cli discord pipewire-pulse
 
 # Install Starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
-# Apply dotfiles
+# Apply configuration
 #
 
 python3 $SCRIPT_DIR/apply.py --link --delete-existing
 cp $SCRIPT_DIR/.xinitrc ~/.xinitrc
+systemctl enable --now --user pipewire-pulse
